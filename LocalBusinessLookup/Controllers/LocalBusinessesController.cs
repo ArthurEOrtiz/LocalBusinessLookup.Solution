d2341118f6d8d166913a6f7086d5960d4254ec0f
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-09-10 15:08:37 -0600

    built LocalBusniessesController

---
+++ LocalBusinessLookup/Controllers/LocalBusinessesController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LocalBusinessLookup.Models;
+
+namespace LocalBusinessLookup.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class LocalBusinessesController : ControllerBase
+  {
+    private readonly LocalBusinessLookupContext _db;
+
+    public LocalBusinessesController(LocalBusinessLookupContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<LocalBusiness>>> Get()
+    {
+      return await _db.LocalBusinesses.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<LocalBusiness>> Post(LocalBusiness business)
+    {
+      _db.LocalBusinesses.Add(business);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction("Post", new { id = business.LocalBusinessId }, business);
+    }
+  }
+}
---
+++ LocalBusinessLookup/Migrations/20220910205535_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalBusinessLookup.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LocalBusinesses",
+                columns: table => new
+                {
+                    LocalBusinessId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Description = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Address = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocalBusinesses", x => x.LocalBusinessId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LocalBusinesses");
+        }
+    }
+}
