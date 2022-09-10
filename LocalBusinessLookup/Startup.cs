7061572accc72ae8f67d09b5c6940cc997689145
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-09-10 14:06:04 -0600

    Framework Core bug
    will not load framework core to other pages.

--- LocalBusinessLookup/Startup.cs
+++ LocalBusinessLookup/Startup.cs
@@ -1,16 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.HttpsPolicy;
+// using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.Extensions.Logging;
+//using Microsoft.OpenApi.Models;
+using LocalBusinessLookup.Models;
 
 namespace LocalBusinessLookup
 {
@@ -27,11 +30,9 @@ namespace LocalBusinessLookup
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "LocalBusinessLookup", Version = "v1" });
-            });
+            services.AddDbContext<LocalBusinessLookupContext>(opt =>
+                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServiceProviderServiceExtensions.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+                services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,11 +41,11 @@ namespace LocalBusinessLookup
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LocalBusinessLookup v1"));
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LocalBusinessLookup v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
