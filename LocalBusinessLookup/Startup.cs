a81dbfc377245d6434af557624137af2098a7082
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-09-10 18:32:37 -0600

    Added Swagger and readme

--- LocalBusinessLookup/Startup.cs
+++ LocalBusinessLookup/Startup.cs
@@ -33,16 +33,28 @@ namespace LocalBusinessLookup
             services.AddDbContext<LocalBusinessLookupContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
                 services.AddControllers();
+                services.AddSwaggerGen(s =>
+                {
+                    s.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Version = "V1",
+                        Title = "Local Buisness API",
+                        Description = "API for local businesses"
+                    });
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>{
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Local Buisnesses API");
+                c.RoutePrefix = string.Empty;
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LocalBusinessLookup v1"));
             }
 
             //app.UseHttpsRedirection();
---
+++ ReadMe.md
@@ -0,0 +1,27 @@
+# Local Business Look up
+
+_Arthur Ortiz_
+
+#### Technologies Used
+* C#
+* .Net 5.0
+* Swagger
+* MySQL
+* MVC
+
+#### Description
+An Api that returns local businesses
+
+#### Setup
+
+1) Clone this respostiry
+2) Build and retore project using CLI
+3) Run Project using CLI
+
+#### Known Bugs
+
+* no known bugs
+
+#### Licence 
+
+Copyright (c) Sept 10, 2022 Arthur Edward Ortiz, MIT
